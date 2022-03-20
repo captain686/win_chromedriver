f6ef57627fab2a95a2320369e1e4ec2099a62756
captain_686 <68617972@qq.com>
2022-03-20 13:30:50 +0800

    update

--- chromedriver.py
+++ chromedriver.py
@@ -1,11 +1,29 @@
+import difflib
 import requests
 import winreg  # 和注册表交互
 import re
 import zipfile
-import os
+import os, re
 
 requests.packages.urllib3.disable_warnings()
 
+
+header = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'zh-CN,zh;q=0.9',
+                'cache-control': 'no-cache',
+                'pragma': 'no-cache',
+                'referer': 'https://chromedriver.storage.googleapis.com/index.html',
+                'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
+                'x-client-data': 'CIe2yQEIo7bJAQjBtskBCKmdygEI/I/LAQjr8ssBCJ75ywEI5oTMAQjzmswBCMqbzAEIm5zMAQ=='}
 
 def getChromeVersion():
     try:
@@ -23,61 +41,73 @@ def getChromeVersion():
 
 def install(paths):
     file_name = "chromedriver_win32.zip"
-    download(file_name)
-    zip_file = zipfile.ZipFile(file_name)
-    for names in zip_file.namelist():
-        for path in paths:
-            print("[*] try to unzip file [%s] to path [%s]" % (file_name, path))
-            zip_file.extract(names, path)
-            print("[o] finish to unzip file [%s] to path [%s]" % (
-        file_name, path))
-    zip_file.close()
-    os.remove(file_name)
-    print("[!] finish to delete the temp file path [%s]" % file_name)
-    print("[*] Start Check")
-    try:
-        check()
-    except Exception as e:
-        print("安装失败,错误信息{}".format(e))
-        
-
-
-def download(file_name):
     version = getChromeVersion()
+    download_status = download(file_name, version)
+    if download_status:
+        zip_file = zipfile.ZipFile(file_name)
+        for names in zip_file.namelist():
+            for path in paths:
+                print("[*] try to unzip file [%s] to path [%s]" % (file_name, path))
+                zip_file.extract(names, path)
+                print("[o] finish to unzip file [%s] to path [%s]" % (
+            file_name, path))
+        zip_file.close()
+        os.remove(file_name)
+        print("[!] finish to delete the temp file path [%s]" % file_name)
+        print("[*] Start Check")
+        try:
+            check()
+        except Exception as e:
+            print("安装失败,错误信息{}".format(e))
+    else:
+        print("install fail")
+        
+def version_similar(version, prefixe):
+    return difflib.SequenceMatcher(None, version, prefixe).quick_ratio()
+
+def getPrefix(version):
+    response = requests.get("https://chromedriver.storage.googleapis.com/?delimiter=/&prefix=")
+    if response.status_code == 200:
+        html = response.text
+        prefixes = re.findall("<CommonPrefixes><Prefix>(.*?)/</Prefix></CommonPrefixes>",html)
+        for prefixe in prefixes:
+            if version_similar(version, prefixe) > 0.83:
+                return prefixe
+    else:
+        return ""
+
+
+def download(file_name, version):
     if version:
+        print("开始下载")
         url = 'https://chromedriver.storage.googleapis.com/{}/chromedriver_win32.zip'.format(version)
-        header = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'accept-encoding': 'gzip, deflate, br',
-                'accept-language': 'zh-CN,zh;q=0.9',
-                'cache-control': 'no-cache',
-                'pragma': 'no-cache',
-                'referer': 'https://chromedriver.storage.googleapis.com/index.html',
-                'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-platform': '"Windows"',
-                'sec-fetch-dest': 'document',
-                'sec-fetch-mode': 'navigate',
-                'sec-fetch-site': 'same-origin',
-                'sec-fetch-user': '?1',
-                'upgrade-insecure-requests': '1',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
-                'x-client-data': 'CIe2yQEIo7bJAQjBtskBCKmdygEI/I/LAQjr8ssBCJ75ywEI5oTMAQjzmswBCMqbzAEIm5zMAQ=='}
         try:
             response = requests.get(url, headers=header, verify=False, timeout=15)
             if response.status_code == 200:
                 with open(file_name,"wb")as f:
                     f.write(response.content)
+                print("下载完成")
+                return True
+            elif response.status_code == 404:
+                print("暂无此版本，尝试使用替代版本")
+                prefix = getPrefix(version)
+                if prefix:
+                    download(file_name, prefix)
+                    return True
+                return ""
         except Exception as e:
             print(e)
+            return False
     else:
         print("未查询到您的Chrome版本,请检查您的Chrome是否正确安装")
+        return False
 
 def check():
     try:
         from selenium import webdriver
     except:
-        os.popen("pip install selenium")
         print("未安装selenium库,开始安装selenium库")
+        os.system("pip install selenium")
         from selenium import webdriver
     
     import time
@@ -91,7 +121,7 @@ def check():
 
 if __name__ == "__main__":
     paths = []
-    path = input("请输入您的python安装目录,并确保其在环境变量中 >>")
+    path = input("请输入您的python安装目录,并确保其在环境变量中 >> ")
     if path:
         paths.append(path.replace("\\","/"))
         print("您的安装目录为：{}".format(paths[0]))
